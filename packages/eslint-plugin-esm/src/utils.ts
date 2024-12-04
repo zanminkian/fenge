d5f1cabf5f25542377926e3a1f6066ee631e4b1b
zanminkian <hellozmj@qq.com>
2024-12-04 08:09:19 +0000

    refactor: remove eslint-disable

--- packages/eslint-plugin-esm/src/utils.ts
+++ packages/eslint-plugin-esm/src/utils.ts
@@ -1,12 +1,15 @@
 /**
  * Like [lodash.memoize](https://lodash.com/docs/4.17.15#memoize)
+ * Type `Res` must be non-nullable, otherwise it will cause bug.
  */
-export function memoize<Arg, Res>(fn: (arg: Arg) => Res): (arg: Arg) => Res {
+export function memoize<Arg, Res extends NonNullable<unknown>>(
+  fn: (arg: Arg) => Res,
+): (arg: Arg) => Res {
   const cache = new Map<Arg, Res>(); // memory leak
   return (arg: Arg) => {
-    if (cache.has(arg)) {
-      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-      return cache.get(arg)!;
+    const cachedResult = cache.get(arg);
+    if (cachedResult !== undefined) {
+      return cachedResult;
     }
     const result = fn(arg);
     cache.set(arg, result);
