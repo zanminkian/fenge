ba3e2f55e1bbe4a065d6482c454b9a9261a1dda2
zanminkian <hellozmj@qq.com>
2025-02-26 04:56:45 +0000

    feat(eslint-config): add rule `@typescript-eslint/no-unnecessary-boolean-literal-compare`

--- packages/eslint-config/src/config/ts/base.ts
+++ packages/eslint-config/src/config/ts/base.ts
@@ -180,7 +180,7 @@ export function getTsBase() {
       "@typescript-eslint/no-non-null-assertion": "error",
       "@typescript-eslint/no-redundant-type-constituents": "error",
       "@typescript-eslint/no-require-imports": "error",
-      // "@typescript-eslint/no-unnecessary-boolean-literal-compare": "error", // unsafe when `strictNullChecks` in tsconfig.json is disabled
+      "@typescript-eslint/no-unnecessary-boolean-literal-compare": "error",
       "@typescript-eslint/no-unnecessary-condition": "error",
       "@typescript-eslint/no-unnecessary-parameter-property-assignment":
         "error",
