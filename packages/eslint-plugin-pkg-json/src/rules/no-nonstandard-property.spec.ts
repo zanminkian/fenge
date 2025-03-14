0d630e79db791d364fe4ebc1b4960e90a3e2da4e
zanminkian <hellozmj@qq.com>
2025-03-14 19:44:42 +0000

    feat(eslint-plugin-pkg-json): disallow `packageManager` for `no-nonstandard-property` rule

--- packages/eslint-plugin-pkg-json/src/rules/no-nonstandard-property.spec.ts
+++ packages/eslint-plugin-pkg-json/src/rules/no-nonstandard-property.spec.ts
@@ -5,12 +5,13 @@ const s = JSON.stringify;
 
 const valid = [
   s({}),
-  s({ name: "foo", type: "foo", config: "foo", packageManager: "foo" }),
+  s({ name: "foo", type: "foo", config: "foo" }),
   s({ dependencies: { foo: "foo" }, config: { bar: "bar" } }),
 ];
 const invalid = [
   s({ name: "", foo: "foo", bar: "bar" }),
   s({ author: "", public: true, pnpm: {} }),
+  s({ name: "foo", type: "foo", yarn: "foo", packageManager: "foo" }),
 ];
 
 await test({ name, rule, valid, invalid, errors: 2 });
