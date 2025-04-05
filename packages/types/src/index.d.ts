1cdd1628a8664253da2e61f7b6abc3f1a867d631
zanminkian <hellozmj@qq.com>
2025-04-05 11:36:39 +0000

    chore: remove

--- packages/types/src/index.d.ts
+++
@@ -1,6 +0,0 @@
-import "./array-is-array.d.ts";
-import "./json-parse.d.ts";
-import "./new-map.d.ts";
-import "./new-promise-reject.d.ts";
-import "./promise-catch.d.ts";
-import "./promise-then.d.ts";
--- packages/types/src/new-map.d.ts
+++
@@ -1,4 +0,0 @@
-/* eslint-disable @typescript-eslint/prefer-function-type */
-interface MapConstructor {
-  new <K = unknown, V = unknown>(): Map<K, V>;
-}
