e200e01f9005f158399804a4c3580241e2ae95be
zanminkian <hellozmj@qq.com>
2025-10-09 08:09:09 +0000

    feat: disallow some libs for `no-restricted-deps` rules

---
+++ .changeset/mighty-phones-fix.md
@@ -0,0 +1,5 @@
+---
+"eslint-plugin-pkg-json": patch
+---
+
+feat: disallow some libs for `no-restricted-deps` rules
--- packages/eslint-plugin-pkg-json/src/rules/no-restricted-deps.js
+++ packages/eslint-plugin-pkg-json/src/rules/no-restricted-deps.js
@@ -19,26 +19,41 @@ export const rule = {
     ],
     defaultOptions: [
       {
+        // fs
         "graceful-fs": "Use built-in `node:fs` module instead.",
         "fs-extra": "Use built-in `node:fs` module instead.",
 
+        // fetch
         "node-fetch": "Use built-in `fetch` api instead.",
         axios: "Use built-in `fetch` api instead.",
         got: "Use built-in `fetch` api instead.",
 
-        // TODO: Uncomment this when we drop support for Node v18.
+        // TODO: Uncomment these when we drop support for Node v18.
+        // util.styleText
         // Why no chalk? Because chalk has some features that built-in styleText does not support.
         // picocolors: "Use built-in util.styleText api instead.",
         // yoctocolors: "Use built-in util.styleText api instead.",
         // "ansi-colors": "Use built-in util.styleText api instead.",
         // colorette: "Use built-in util.styleText api instead.",
+        // kleur: "Use built-in util.styleText api instead.",
 
+        // TODO: Uncomment these when we drop support for Node v20.
+        // glob
+        // glob: "Use built-in `fs.glob` api instead.",
+        // globby: "Use built-in `fs.glob` api instead.",
+        // "fast-glob": "Use built-in `fs.glob` api instead.",
+        // "tiny-glob": "Use built-in `fs.glob` api instead.",
+        // tinyglobby: "Use built-in `fs.glob` api instead.",
+
+        // module.findPackageJSON
         "package-up": "Use built-in `module.findPackageJSON` api instead.",
         "read-package-up": "Use built-in `module.findPackageJSON` api instead.",
 
+        // others
         "import-meta-resolve":
           "Use built-in `import.meta.resolve` api or built-in `node:module` instead.",
         lodash: "Use modern ES6+ apis instead.",
+        uuid: "Use built-in `crypto.randomUUID` api instead.",
       },
     ],
   },
