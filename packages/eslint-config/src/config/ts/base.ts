9db6f045666b99f3ce47e30e04c94960514f1486
zanminkian <hellozmj@qq.com>
2025-10-22 08:20:26 +0000

    feat(eslint-config): disallow `namespace` in definition files

--- packages/eslint-config/src/config/ts/base.ts
+++ packages/eslint-config/src/config/ts/base.ts
@@ -181,7 +181,10 @@ export function getTsBase() {
       "@typescript-eslint/no-misused-promises": "error",
       "@typescript-eslint/no-misused-spread": "error",
       "@typescript-eslint/no-mixed-enums": "error",
-      "@typescript-eslint/no-namespace": "error", // consider to add option `{"allowDefinitionFiles": false}` to strictly forbid `namespace` keyword
+      "@typescript-eslint/no-namespace": [
+        "error",
+        { allowDefinitionFiles: false },
+      ],
       "@typescript-eslint/no-non-null-asserted-nullish-coalescing": "error",
       "@typescript-eslint/no-non-null-asserted-optional-chain": "error",
       "@typescript-eslint/no-non-null-assertion": "error",
